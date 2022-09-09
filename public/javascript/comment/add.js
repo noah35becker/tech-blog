37e1c2c56c6503809270aadb016e13036689b8d4
Noah Becker <102703954+noah35becker@users.noreply.github.com>
2022-09-09 17:23:36 -0400

    add comment functionality added

---
+++ public/javascript/comment/add.js
@@ -0,0 +1,44 @@
+
+// GLOBAL VARIABLES
+const addCommentWrapperEl = $('.add-comment-wrapper');
+const newCommentEl = $('<textarea id="new-comment" placeholder="new comment" />');
+
+const postId = +$('article.post').attr('post-id');
+
+
+
+// FUNCTIONS
+
+function addCommentBtnHandler(){
+    addCommentWrapperEl.prepend(newCommentEl);
+
+    $('.add-comment-btn')
+        .removeClass('add-comment-btn')
+        .addClass('save-new-comment-btn')
+        .text('Post new comment');
+}
+
+
+async function saveNewCommentBtnHandler(){
+    const newComment = newCommentEl.val().trim();
+
+    const response = await fetch('/api/comment',{
+        method: 'post',
+        body: JSON.stringify({
+            content: newComment,
+            post_id: postId
+        }),
+        headers: {'Content-Type': 'application/json'}
+    });
+
+    if (response.ok)
+        document.location.reload();
+    else
+        alert(response.statusText);
+}
+
+
+
+// EVENT LISTENERS
+$('.add-comment-wrapper').on('click', '.add-comment-btn', addCommentBtnHandler);
+$('.add-comment-wrapper').on('click', '.save-new-comment-btn', saveNewCommentBtnHandler);
