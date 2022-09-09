a4cd5fc64963dec3f40989acc14accc942f2a85c
Noah Becker <102703954+noah35becker@users.noreply.github.com>
2022-09-09 12:40:30 -0400

    display comments counter on each post

--- utils/handlebars-helpers.js
+++ utils/handlebars-helpers.js
@@ -4,5 +4,7 @@ const {DateTime} = require('luxon');
 module.exports = {
     format_date: date => {
         return DateTime.fromISO(new Date(date).toISOString()).toFormat("MMM d, yyyy 'at' HH:mm:ss");
-    }
+    },
+
+    format_plural: (word, quantity) => quantity === 1 ? word : word+'s'
 };
